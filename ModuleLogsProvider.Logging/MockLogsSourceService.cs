534d0ad41967ff9bcc23685b348896c830325fa6
mikhail <mikhail@localhost>
2011-09-25 10:49:36 +0400

    ?????????? ? ??????????? ?????????? ???????, ??????? ? ?????? ? ????????.

---
+++ ModuleLogsProvider.Logging/MockLogsSourceService.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Awad.Eticket.ModuleLogsProvider.Types;
+using ILogSourceService = ModuleLogsProvider.Logging.MostLogsServices.ILogSourceService;
+
+namespace ModuleLogsProvider.Logging
+{
+	public sealed class MockLogsSourceService : ILogSourceService
+	{
+		private readonly List<LogMessageInfo> messages = new List<LogMessageInfo>();
+		private bool isListening = true;
+
+		public void AddMessage( LogMessageInfo message )
+		{
+			messages.Add( message );
+		}
+
+		#region ILogSourceService Members
+
+		public void ClearMessagesList()
+		{
+			messages.Clear();
+		}
+
+		public void StartListening()
+		{
+			isListening = true;
+		}
+
+		public void StopListening()
+		{
+			isListening = false;
+		}
+
+		public bool GetIsListening()
+		{
+			return isListening;
+		}
+
+		public LogMessageInfo[] GetLinesStartingWithIndex( int index )
+		{
+			var result = messages.Skip( index ).ToArray();
+			return result;
+		}
+
+		#endregion
+	}
+}
