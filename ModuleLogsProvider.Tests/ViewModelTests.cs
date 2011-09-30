eab3a9a02d61575ab390f35333a7b469a32ae1c9
mikhail <mikhail@localhost>
2011-09-30 19:06:12 +0400

    ???? ?? ??????????? ? ?????????? ????? ??????? ??? ?????????? ?????? ????? ????????.
    ? ?????????? ??????.

--- ModuleLogsProvider.Tests/ViewModelTests.cs
+++ ModuleLogsProvider.Tests/ViewModelTests.cs
@@ -3,10 +3,12 @@ using System.Collections.Specialized;
 using System.Linq;
 using System.Reactive.Concurrency;
 using System.Reactive.Linq;
+using LogAnalyzer;
 using LogAnalyzer.Config;
 using LogAnalyzer.GUI.ViewModels;
 using LogAnalyzer.Kernel;
 using LogAnalyzer.Operations;
+using LogAnalyzer.Tests;
 using ModuleLogsProvider.Logging.Mocks;
 using ModuleLogsProvider.Logging.Most;
 using ModuleLogsProvider.Logging.MostLogsServices;
@@ -41,6 +43,16 @@ namespace ModuleLogsProvider.Tests
 			var firstDirectory = appViewModel.Core.Directories.First();
 			var dirMergedEntriesCollectionChanged = firstDirectory.MergedEntries.CreateEventCounterFromCollectionChanged();
 
+			var mergedEntriesGroupEventCounter = new CompositeEventCounter( dirMergedEntriesCollectionChanged,
+																		   dirViewModelMergedEntriesCollectionChanged );
+
+			var firstDirFilesCollectionChangedEventCounter = firstDirectory.Files.CreateEventCounterFromCollectionChanged();
+			var firstDirViewModelFilesCollectionChangedEventCounter =
+				firstDirectoryViewModel.Files.CreateEventCounterFromCollectionChanged();
+
+			var fileChangedFroupEventCounter = new CompositeEventCounter( firstDirFilesCollectionChangedEventCounter,
+																		 firstDirViewModelFilesCollectionChangedEventCounter );
+
 			service.AddMessage( new LogMessageInfo { MessageType = "E", Message = "[E] [ 69] 24.05.2011 0:00:12	Message1", LoggerName = LoggerName } );
 			timer.MakeRing();
 
@@ -50,20 +62,25 @@ namespace ModuleLogsProvider.Tests
 
 			Assert.AreEqual( 1, firstFile.LogEntries.Count );
 			Assert.AreEqual( 1, firstDirectoryViewModel.MergedEntries.Count );
+
+			Assert.IsTrue( fileChangedFroupEventCounter.HaveBeenInvokedOneTime() );
+
+			Assert.IsTrue( mergedEntriesGroupEventCounter.HaveBeenInvokedOneTime() );
 			Assert.AreEqual( 1, dirMergedEntriesCollectionChanged.CalledTimes );
 			Assert.AreEqual( 1, dirViewModelMergedEntriesCollectionChanged.CalledTimes );
 		}
 
-		private Init()
+		[TestFixtureSetUp]
+		public void Init()
 		{
-			var serverFactory = new MockLogSourceFactory(service);
+			var serverFactory = new MockLogSourceFactory( service );
 			var scheduler = OperationScheduler.SyncronousScheduler;
 
-			config = EnvironmentTestHelper.BuildConfig(timer, serverFactory, scheduler, queue);
-			config.RegisterInstance<IScheduler>(Scheduler.Immediate);
+			this.config = EnvironmentTestHelper.BuildConfig( timer, serverFactory, scheduler, queue );
+			config.RegisterInstance<IScheduler>( Scheduler.Immediate );
 
-			MostEnvironment env = new MostEnvironment(config);
-			config.RegisterInstance<IEnvironment>(env);
+			MostEnvironment env = new MostEnvironment( config );
+			config.RegisterInstance<IEnvironment>( env );
 		}
 	}
 }
