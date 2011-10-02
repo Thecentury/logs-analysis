0cbbb4837d893ea628bd6249e02e1615059bd977
Mikhail <Mikhail@localhost>
2011-10-02 22:00:48 +0400

    1. ???????? ??????? ????????????? ????????? 2. ???????? ????????? ?????????? ???????????? ??????????? ????????? ? ListView.

--- LogAnalyzer/ViewModels/Collections/DispatcherObservableCollection.cs
+++ LogAnalyzer/ViewModels/Collections/DispatcherObservableCollection.cs
@@ -10,6 +10,10 @@ using System.ComponentModel;
 namespace LogAnalyzer.GUI.ViewModels.Collections
 {
 	/// <summary>
+	using System;
+	using System.Linq;
+
+	/// <summary>
 	/// Передает событие CollectionChanged в UI поток.
 	/// </summary>
 	[IgnoreMissingProperty( "Count" )]
@@ -26,8 +30,14 @@ namespace LogAnalyzer.GUI.ViewModels.Collections
 
 			INotifyCollectionChanged observableCollection = (INotifyCollectionChanged)collection;
 
-			Observable.FromEventPattern<NotifyCollectionChangedEventArgs>( observableCollection, "CollectionChanged" )
-				.ObserveOn( scheduler )
+			var collectionChanged = Observable.FromEventPattern<NotifyCollectionChangedEventArgs>( observableCollection, "CollectionChanged" )
+				.ObserveOn( scheduler );
+
+			collectionChanged.Where( e => e.EventArgs.Action == NotifyCollectionChangedAction.Add )
+				.SelectMany( e => e.EventArgs.NewItems.OfType<object>() )
+				.Subscribe( addedItem => OnCollectionChanged( new NotifyCollectionChangedEventArgs( NotifyCollectionChangedAction.Add, addedItem ) ) );
+
+			collectionChanged.Where( e => e.EventArgs.Action != NotifyCollectionChangedAction.Add )
 				.Subscribe( e => OnCollectionChanged( e.EventArgs ) );
 		}
 
