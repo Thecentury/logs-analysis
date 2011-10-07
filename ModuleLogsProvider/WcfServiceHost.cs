7bf9cb43dbd0ac7d74b1a2c46530fa816f87d7aa
Mikhail Brinchuk <3943804+Thecentury@users.noreply.github.com>
2011-10-07 23:36:32 +0400

    + MOST in Ext

---
+++ ModuleLogsProvider/WcfServiceHost.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using Morqua.Logging;
+using Awad.Eticket.ModuleLogsProvider.Types;
+
+namespace Awad.Eticket.ModuleLogsProvider
+{
+	internal sealed class WcfServiceHost : IDisposable
+	{
+		private ServiceHost serviceHost;
+
+		private readonly ILogger logger;
+		public WcfServiceHost( ILogger logger )
+		{
+			if ( logger == null ) throw new ArgumentNullException( "logger" );
+
+			this.logger = logger;
+		}
+
+		public void Start( MostLogSourceService serviceInstance )
+		{
+			try
+			{
+				const string uri = "http://127.0.0.1:9999/MostLogSourceService/";
+
+				serviceHost = new ServiceHost( serviceInstance );
+
+				serviceHost.Faulted += HostFaulted;
+				serviceHost.UnknownMessageReceived += HostUnknownMessageReceived;
+				serviceHost.Opening += HostOpening;
+				serviceHost.Opened += HostOpened;
+				serviceHost.Closing += HostClosing;
+				serviceHost.Closed += HostClosed;
+
+				serviceHost.AddServiceEndpoint(
+					typeof( ILogSourceService ),
+					new BasicHttpBinding(),
+					uri
+					);
+
+				serviceHost.Open();
+			}
+			catch ( Exception exc )
+			{
+				logger.WriteLine( MessageType.Error, string.Format( "WcfServiceHost.Start: Exc = {0}", exc ) );
+				throw;
+			}
+		}
+
+		public MostLogSourceService GetInstance()
+		{
+			MostLogSourceService service = (MostLogSourceService)serviceHost.SingletonInstance;
+			return service;
+		}
+
+		void HostClosed( object sender, EventArgs e )
+		{
+			logger.WriteLine( MessageType.Info, "WcfServiceHost.Closed()" );
+		}
+
+		void HostClosing( object sender, EventArgs e )
+		{
+			logger.WriteLine( MessageType.Info, "WcfServiceHost.Closing()" );
+		}
+
+		void HostOpened( object sender, EventArgs e )
+		{
+			logger.WriteLine( MessageType.Info, "WcfServiceHost.Opened()" );
+		}
+
+		void HostOpening( object sender, EventArgs e )
+		{
+			logger.WriteLine( MessageType.Info, "WcfServiceHost.Opening()" );
+		}
+
+		void HostUnknownMessageReceived( object sender, UnknownMessageReceivedEventArgs e )
+		{
+			logger.WriteLine( MessageType.Warning, "WcfServiceHost.UnknownMessageReceived()" );
+		}
+
+		void HostFaulted( object sender, EventArgs e )
+		{
+			logger.WriteLine( MessageType.Warning, "WcfServiceHost.Faulted()" );
+		}
+
+		public void Dispose()
+		{
+			try
+			{
+				if ( serviceHost != null )
+				{
+					if ( serviceHost.State != CommunicationState.Faulted )
+					{
+						serviceHost.Close();
+					}
+				}
+			}
+			catch ( Exception exc )
+			{
+				logger.WriteLine( MessageType.Warning, "WcfServiceHost.Dispose(): Exception {0}", exc );
+			}
+		}
+	}
+}
