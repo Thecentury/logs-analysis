7423ab0bfc94a77ec81189ba010660496df65928
Mikhail Brinchuk <3943804+Thecentury@users.noreply.github.com>
2011-10-09 18:35:54 +0400

    + ??????? ServiceBehavior

--- ModuleLogsProvider.Interfaces/CurrentProcessPerformanceService.cs
+++ ModuleLogsProvider.Interfaces/CurrentProcessPerformanceService.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 
 namespace ModuleLogsProvider.Interfaces
 {
+	[ServiceBehavior( InstanceContextMode = InstanceContextMode.Single )]
 	public sealed class CurrentProcessPerformanceService : IPerformanceInfoService
 	{
 		private readonly CpuLoadHelper cpuLoadHelper = new CpuLoadHelper();
