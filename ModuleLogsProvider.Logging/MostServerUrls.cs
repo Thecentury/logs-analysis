e1d6440bc3155b9221416f1bbf54169ec87a1fd3
mikhail <mikhail@localhost>
2011-10-09 20:31:23 +0400

    Most config

---
+++ ModuleLogsProvider.Logging/MostServerUrls.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ModuleLogsProvider.Logging
+{
+	public sealed class MostServerUrls
+	{
+		public string DisplayName { get; set; }
+
+		public string LogsSourceServiceUrl { get; set; }
+
+		public string LogsSinkServiceUrl { get; set; }
+
+		public string PerformanceDataServiceUrl { get; set; }
+	}
+}
