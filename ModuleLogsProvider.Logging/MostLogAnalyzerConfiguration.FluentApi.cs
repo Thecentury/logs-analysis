e0730290d9ffa50a159cf26f34492ecd99e39472
Mikhail Brinchuk <3943804+Thecentury@users.noreply.github.com>
2011-10-12 22:56:25 +0400

    ??????? ?????? ?????????? - ???????????? ???????.

---
+++ ModuleLogsProvider.Logging/MostLogAnalyzerConfiguration.FluentApi.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ModuleLogsProvider.Logging
+{
+	public static class MostLogAnalyzerConfigurationExtensions
+	{
+		public static T WithLogsUpdateTimer<T>(this T config, ITimer logsUpdateTimer) where T: MostLogAnalyzerConfiguration
+		{
+			if (logsUpdateTimer == null) throw new ArgumentNullException("logsUpdateTimer");
+
+			config.LogsUpdateTimer = logsUpdateTimer;
+			return config;
+		}
+
+		public static T WithPerformanceDataUpdateTimer<T>(this T config, ITimer performanceDataUpdateTimer) where T:MostLogAnalyzerConfiguration
+		{
+			if (performanceDataUpdateTimer == null) throw new ArgumentNullException("performanceDataUpdateTimer");
+
+			config.PerformanceDataUpdateTimer = performanceDataUpdateTimer;
+			return config;
+		}
+	}
+}
