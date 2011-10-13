d9ac1fb4511a12403f7847c50ed55094450aa9c5
Mikhail <Mikhail@localhost>
2011-10-13 18:13:36 +0400

    ?????????? ????? ????????.

--- LogAnalyzer.Most.App/MostAppBootstrapper.cs
+++ LogAnalyzer.Most.App/MostAppBootstrapper.cs
@@ -13,7 +13,9 @@ using LogAnalyzer.Kernel;
 using LogAnalyzer.Logging;
 using ModuleLogsProvider.GUI.ViewModels;
 using ModuleLogsProvider.Logging;
+using ModuleLogsProvider.Logging.Auxilliary;
 using ModuleLogsProvider.Logging.Most;
+using ModuleLogsProvider.Logging.MostLogsServices;
 
 namespace LogAnalyzer.Most.App
 {
@@ -44,8 +46,7 @@ namespace LogAnalyzer.Most.App
 
 		private void Init()
 		{
-			const string logsServiceAddress = "net.tcp://127.0.0.1:9999/MostLogSourceService/";
-			MostServerLogSourceFactory serviceFactory = new MostServerLogSourceFactory( logsServiceAddress );
+			MostServiceFactory<ILogSourceService> serviceFactory = new MostServiceFactory<ILogSourceService>( new NetTcpBindingFactory(), MostServerUrls.Local.LogsSourceServiceUrl );
 
 			const string dirName = "MOST";
 			const string filesFilter = "*";
@@ -63,7 +64,7 @@ namespace LogAnalyzer.Most.App
 				.WithLogsUpdateTimer( new WpfDispatcherTimer( TimeSpan.FromSeconds( 20 ) ) )
 				.WithPerformanceDataUpdateTimer( new WpfDispatcherTimer( TimeSpan.FromSeconds( 2 ) ) )
 				.SetSelectedUrls( MostServerUrls.Local )
-				.RegisterInstance<ILogSourceServiceFactory>( serviceFactory )
+				.RegisterInstance<IFactory<IDisposableService<ILogSourceService>>>( serviceFactory )
 				.RegisterInstance<IOperationsQueue>( operationsQueue )
 				.RegisterInstance<IWindowService>( new RealWindowService() )
 				.RegisterInstance<IErrorReportingService>( new LoggingErrorReportingService( Logger ) );
