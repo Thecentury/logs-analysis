b827a6c777997d26b1a6d6f6156aaf6a14a9a67a
mikhail <mikhail@localhost>
2011-10-19 21:56:42 +0400

    ??????? ??? ?????????? ?? Bootstrapper'?.

--- LogAnalyzer.Most.App/MostAppBootstrapper.cs
+++ LogAnalyzer.Most.App/MostAppBootstrapper.cs
@@ -1,53 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Windows;
 using LogAnalyzer.Config;
 using LogAnalyzer.Extensions;
-using LogAnalyzer.GUI.Common;
+using LogAnalyzer.GUI;
 using LogAnalyzer.GUI.Regions;
-using LogAnalyzer.GUI.ViewModels;
-using LogAnalyzer.Kernel;
-using LogAnalyzer.Logging;
-using LogAnalyzer.Operations;
 using ModuleLogsProvider.GUI.ViewModels;
 using ModuleLogsProvider.GUI.Views;
 using ModuleLogsProvider.Logging;
-using ModuleLogsProvider.Logging.Auxilliary;
 using ModuleLogsProvider.Logging.Most;
 using ModuleLogsProvider.Logging.MostLogsServices;
 
 namespace LogAnalyzer.Most.App
 {
-	public sealed class MostAppBootstrapper
+	public sealed class MostAppBootstrapper : Bootstrapper
 	{
-		private Logger logger;
-		public Logger Logger
-		{
-			get { return logger; }
-		}
-
-		private string[] commandLineArgs;
-
-		public void Start( string[] commandLineArgs )
-		{
-			this.commandLineArgs = commandLineArgs;
-
-			Thread.CurrentThread.Name = "UIThread";
-
-			Task.Factory
-				.StartNew( Init )
-				.ContinueWith( OnInitException, TaskContinuationOptions.OnlyOnFaulted );
-		}
-
 		private MostServerUrls GetSelectedUrls( MostLogAnalyzerConfiguration config )
 		{
 			MostServerUrls result = MostServerUrls.Local;
 
-			string selectedServerString = commandLineArgs.FirstOrDefault( line => line.StartsWith( "-server:" ) );
+			string selectedServerString = CommandLineArgs.FirstOrDefault( line => line.StartsWith( "-server:" ) );
 			if ( selectedServerString != null )
 			{
 				string[] parts = selectedServerString.Split( ':' );
@@ -59,7 +31,7 @@ namespace LogAnalyzer.Most.App
 			return result;
 		}
 
-		private void Init()
+		protected override void Init()
 		{
 			MostServiceFactory<ILogSourceService> serviceFactory = new MostServiceFactory<ILogSourceService>(
 				new NetTcpBindingFactory(), MostServerUrls.Local.LogsSourceServiceUrl );
@@ -68,15 +40,8 @@ namespace LogAnalyzer.Most.App
 			const string filesFilter = "*";
 			const string displayName = "MOST.Local";
 
-			var config = MostLogAnalyzerConfiguration
-				.LoadFromFile( @"../../config.xaml" )
-				.AcceptAllLogTypes();
-			//.AddLogWriter( new DebugLogWriter() );
-
-			logger = config.Logger;
-			var operationsQueue = new WorkerThreadOperationsQueue( logger );
-
-			RegionManager regionManager = new RegionManager();
+			var config = MostLogAnalyzerConfiguration.LoadFromFile( @"../../config.xaml" );
+			InitConfig( config );
 
 			MostServerUrls serverUrls = GetSelectedUrls( config );
 
@@ -86,37 +51,21 @@ namespace LogAnalyzer.Most.App
 				.WithPerformanceDataUpdateTimer( new WpfDispatcherTimer( TimeSpan.FromSeconds( 2 ) ) )
 				.SetSelectedUrls( serverUrls )
 				.Register<ITimer>( () => new WpfDispatcherTimer() )
-				.RegisterInstance<IServiceFactory<ILogSourceService>>( serviceFactory )
-				.RegisterInstance<IOperationsQueue>( operationsQueue )
-				.RegisterInstance<IWindowService>( new RealWindowService() )
-				.RegisterInstance<OperationScheduler>( OperationScheduler.TaskScheduler )
-				.RegisterInstance<IErrorReportingService>( new ErrorReportingService() )
-				.RegisterInstance<RegionManager>( regionManager );
+				.RegisterInstance<IServiceFactory<ILogSourceService>>( serviceFactory );
 
 			config.ViewManager.RegisterView( typeof( PerformanceView ), typeof( ServerPerformanceViewModel ) );
 
 			var environment = new MostEnvironment( config );
 
 			MostApplicationViewModel applicationViewModel = new MostApplicationViewModel( config, environment );
-			Application.Current.Dispatcher.BeginInvoke( () =>
-															{
-																var mainWindow = Application.Current.MainWindow;
+			Application.Current.Dispatcher.BeginInvoke(
+				() =>
+				{
+					var mainWindow = Application.Current.MainWindow;
 
-																RegionManager.SetRegionManager( mainWindow, regionManager );
-																mainWindow.DataContext = applicationViewModel;
-															} );
-		}
-
-		private void OnInitException( Task parentTask )
-		{
-			var exception = parentTask.Exception;
-
-			Logger.WriteError( "Crash. Exception: {0}", exception );
-			Extensions.Condition.BreakIfAttached();
-
-			MessageBox.Show( "Unhandled exception: " + exception, "Unhandled exception", MessageBoxButton.OK, MessageBoxImage.Error );
-
-			Environment.Exit( -1 );
+					RegionManager.SetRegionManager( mainWindow, config.ResolveNotNull<RegionManager>() );
+					mainWindow.DataContext = applicationViewModel;
+				} );
 		}
 	}
 }
