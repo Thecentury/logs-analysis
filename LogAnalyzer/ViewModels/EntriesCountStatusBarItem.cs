b513300fe48bac4d4df3a411d3876de25f7ca917
mikhail <mikhail@localhost>
2011-10-22 18:39:46 +0400

    ?????????

---
+++ LogAnalyzer/ViewModels/EntriesCountStatusBarItem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LogAnalyzer.GUI.ViewModels
+{
+	public class EntriesCountStatusBarItem : BindingObject, ITypeName
+	{
+		private readonly LogEntriesListViewModel parent;
+		internal EntriesCountStatusBarItem( LogEntriesListViewModel parent )
+			: base( parent )
+		{
+			this.parent = parent;
+		}
+
+		public int TotalEntries
+		{
+			get { return parent.TotalEntries; }
+		}
+
+		public string Type
+		{
+			get { return GetType().Name; }
+		}
+	}
+}
