186892929565a312cc149defff9051adbafed90d
Mikhail <Mikhail@localhost>
2011-10-24 19:08:17 +0400

    ?????????

--- LogAnalyzer/Properties/Settings.Designer.cs
+++ LogAnalyzer/Properties/Settings.Designer.cs
@@ -13,31 +13,13 @@ namespace LogAnalyzer.GUI.Properties {
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "10.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+    public sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
         
         public static Settings Default {
             get {
                 return defaultInstance;
-            }
-        }
-        
-        [global::System.Configuration.ApplicationScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("../../../config.xaml")]
-        public string ConfigPath {
-            get {
-                return ((string)(this["ConfigPath"]));
-            }
-        }
-        
-        [global::System.Configuration.ApplicationScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("True")]
-        public bool ShowFilterEditorAtStartup {
-            get {
-                return ((bool)(this["ShowFilterEditorAtStartup"]));
             }
         }
     }
