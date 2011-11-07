053fc9c4c0ca6b9f1db28869759b6692e84358c6
mikhail.brinchuk <mikhail.brinchuk@localhost>
2011-11-07 23:46:50 +0400

    ExcludeFilesByNameFilter

---
+++ LogAnalyzer.Core/Filters/ExcludeFilesByNameFilter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Windows.Markup;
+using LogAnalyzer.Kernel;
+
+namespace LogAnalyzer.Filters
+{
+	[IgnoreBuilder]
+	[ContentProperty( "FileNames" )]
+	public sealed class ExcludeFilesByNameFilter : ExpressionBuilder
+	{
+		private readonly List<string> fileNames = new List<string>();
+		[DesignerSerializationVisibility( DesignerSerializationVisibility.Content )]
+		public List<string> FileNames
+		{
+			get { return fileNames; }
+		}
+
+		public override Type GetResultType( ParameterExpression target )
+		{
+			return typeof( bool );
+		}
+
+		protected override Expression CreateExpressionCore( ParameterExpression parameterExpression )
+		{
+			if ( parameterExpression.Type != typeof( IFileInfo ) )
+				throw new NotSupportedException( "ExcludeFilesByNameFilter is for IFileInfo only." );
+
+			return
+				Expression.Not(
+					Expression.Call(
+						Expression.Constant( fileNames ),
+						typeof( List<string> ).GetMethod( "Contains" ),
+						Expression.Property(
+							parameterExpression, "Name" )
+						)
+					);
+		}
+	}
+}
