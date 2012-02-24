a067705ff6e591975cd478533e7a06271f6ba551
Mikhail Brinchuk <3943804+Thecentury@users.noreply.github.com>
2012-02-24 21:52:11 +0400

    AvalonDock

---
+++ LogAnalyzer/AvalonDocks/AbstractPaneViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace SampleApp.ViewModels
+{
+    /// <summary>
+    /// Abstract base class for an AvalonDock pane view-model.
+    /// </summary>
+    public class AbstractPaneViewModel : AbstractViewModel
+    {
+        /// <summary>
+        /// Set to 'true' when the pane is visible.
+        /// </summary>
+        private bool _isVisible = true;
+
+        /// <summary>
+        /// Set to 'true' when the pane is visible.
+        /// </summary>
+        public bool IsVisible
+        {
+            get
+            {
+                return _isVisible;
+            }
+            set
+            {
+                if (_isVisible == value)
+                {
+                    return;
+                }
+
+                _isVisible = value;
+
+                OnPropertyChanged("IsVisible");
+            }
+        }
+    }
+}
--- LogAnalyzer/Views/TabsView.xaml.cs
+++ LogAnalyzer/Views/TabsView.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Controls;
+﻿using System.IO;
+using System.Windows.Controls;
+using AvalonDockMVVM;
 
 namespace LogAnalyzer.GUI.Views
 {
@@ -10,6 +12,22 @@ namespace LogAnalyzer.GUI.Views
 		public TabsView()
 		{
 			InitializeComponent();
+		}
+
+		private void AvalonDockHost_AvalonDockLoaded( object sender, System.EventArgs e )
+		{
+			string layout =
+			"<DockingManager version=\"1.3.0\">" +
+				"<ResizingPanel ResizeWidth=\"*\" ResizeHeight=\"*\" EffectiveSize=\"0,0\" Orientation=\"Horizontal\">" +
+					"<DocumentPane IsMain=\"true\" ResizeWidth=\"*\" ResizeHeight=\"*\" />" +
+				"</ResizingPanel>" +
+				"<Hidden />" +
+				"<Windows />" +
+			"</DockingManager>";
+
+			AvalonDockHost host = (AvalonDockHost)sender;
+
+			host.DockingManager.RestoreLayout( new StringReader( layout ) );
 		}
 	}
 }
