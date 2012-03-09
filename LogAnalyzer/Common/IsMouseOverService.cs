f6ba79c165bc3b87e48af19854d42747f4bd4a54
Mikhail.Brinchuk <Mikhail.Brinchuk@localhost>
2012-03-09 21:11:36 +0400

    Open file overlay for files tree view

---
+++ LogAnalyzer/Common/IsMouseOverService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace LogAnalyzer.GUI.Common
+{
+	public static class IsMouseOverService
+	{
+		public static bool GetIsMouseOver( DependencyObject obj )
+		{
+			return (bool)obj.GetValue( IsMouseOverProperty );
+		}
+
+		public static void SetIsMouseOver( DependencyObject obj, bool value )
+		{
+			obj.SetValue( IsMouseOverProperty, value );
+		}
+
+		public static readonly DependencyProperty IsMouseOverProperty = DependencyProperty.RegisterAttached(
+		  "IsMouseOver",
+		  typeof( bool ),
+		  typeof( IsMouseOverService ),
+		  new FrameworkPropertyMetadata( false, FrameworkPropertyMetadataOptions.Inherits ) );
+	}
+}
