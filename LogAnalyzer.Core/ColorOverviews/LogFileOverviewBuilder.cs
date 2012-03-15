264793a2116d3431921627beafc12ca2df677303
Mikhail.Brinchuk <Mikhail.Brinchuk@localhost>
2012-03-15 17:15:12 +0400

    LogFileNameOverview

---
+++ LogAnalyzer.Core/ColorOverviews/LogFileOverviewBuilder.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LogAnalyzer.ColorOverviews
+{
+	public sealed class LogFileOverviewBuilder : OverviewBuilderBase<IEnumerable<LogEntry>, LogEntry>
+	{
+		protected override LogEntry GetValue( IEnumerable<LogEntry> logEntries )
+		{
+			LogEntry mostFrequentFileEntry = (from entry in logEntries
+			                                  group entry by entry.ParentLogFile.Name
+			                                  into g
+			                                  let head = new { FileName = g.Key, First = g.First(), Count = g.Count() }
+			                                  orderby head.Count descending
+			                                  select head.First).FirstOrDefault();
+
+			return mostFrequentFileEntry;
+		}
+	}
+}
---
+++ LogAnalyzer/Common/LogFileNameBrushesCache.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Media;
+
+namespace LogAnalyzer.GUI.Common
+{
+	public static class LogFileNameBrushesCache
+	{
+		private static readonly Dictionary<int, Brush> logFileNameBrushesCache = new Dictionary<int, Brush>();
+
+		private static readonly ReaderWriterLockSlim sync = new ReaderWriterLockSlim();
+
+		public static Brush GetBrush( string name )
+		{
+			int hashCode = name.GetHashCode();
+			Brush logNameBackground;
+
+			sync.EnterUpgradeableReadLock();
+
+			if ( !logFileNameBrushesCache.TryGetValue( hashCode, out logNameBackground ) )
+			{
+				sync.EnterWriteLock();
+
+				double hue = (hashCode - (double)Int32.MinValue) / ((double)Int32.MaxValue - Int32.MinValue) * 360;
+				HsbColor hsbColor = new HsbColor( hue, 0.2, 1 );
+				HsbColor darkerColor = new HsbColor( hue, 0.2, 0.95 );
+				logNameBackground = new LinearGradientBrush( hsbColor.ToArgbColor(), darkerColor.ToArgbColor(), 90 );
+				logNameBackground.Freeze();
+
+				logFileNameBrushesCache.Add( hashCode, logNameBackground );
+
+				sync.ExitWriteLock();
+			}
+
+			sync.ExitUpgradeableReadLock();
+
+			return logNameBackground;
+		}
+	}
+}
