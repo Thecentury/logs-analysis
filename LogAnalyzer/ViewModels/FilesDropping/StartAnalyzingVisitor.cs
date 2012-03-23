19ef751a3b522fe3d3c965ec218870c693ae62d7
Mikhail Brinchuk <3943804+Thecentury@users.noreply.github.com>
2012-03-23 18:23:28 +0400

    Fixed notifications on dropped files

--- LogAnalyzer/ViewModels/FilesDropping/StartAnalyzingVisitor.cs
+++ LogAnalyzer/ViewModels/FilesDropping/StartAnalyzingVisitor.cs
@@ -8,21 +8,18 @@ namespace LogAnalyzer.GUI.ViewModels.FilesDropping
 {
 	public sealed class StartAnalyzingVisitor : IDroppedObjectVisitor
 	{
-		private readonly LogDirectory _droppedFilesDirectory;
 		private readonly LogAnalyzerCore _core;
 
-		public StartAnalyzingVisitor( [NotNull] LogDirectory droppedFilesDirectory, [NotNull] LogAnalyzerCore core )
+		public StartAnalyzingVisitor([NotNull] LogAnalyzerCore core )
 		{
-			if ( droppedFilesDirectory == null ) throw new ArgumentNullException( "droppedFilesDirectory" );
 			if ( core == null ) throw new ArgumentNullException( "core" );
 
-			this._droppedFilesDirectory = droppedFilesDirectory;
 			this._core = core;
 		}
 
 		public void Visit( DroppedFileViewModel file )
 		{
-			_droppedFilesDirectory.Files.Add( file.LogFile );
+			//_droppedFilesDirectory.Files.Add( file.LogFile );
 		}
 
 		public void Visit( DroppedDirectoryViewModel directory )
